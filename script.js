34945ced2bb26c93bd613eefb7c868ce7c562a82
Abdullah0505 <>
2021-04-30 22:51:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('click').addEventListener ('click', displayMessage)
+
+function displayMessage () {
+  document.getElementById('paragraph').innerHTML = "Genetic therapy allows for a change in our DNA, whether it involves replacing parts of it or editing parts of it. Gene therapy could also be abused, which is ethically concerning because our DNA is something very precise that is not meant to be toyed with. If this came to light, humanity may no longer be human. Genetic therapy is also risky because DNA is the thing that makes you who you are, if one were to play god with this fact, one may be diagnosed with a disease or damaged beyond repair."
+}
+
+document.getElementById('button').addEventListener ('click', displayText)
+
+function displayText () {
+  alert("Hello")
+}
