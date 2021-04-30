a24bd6b85071d5cbffa3400de02db46695a59c7f
Abdullah0505 <>
2021-04-30 22:58:55 +0000

    edit2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('click').addEventListener('click', displayMessage)
 
 function displayMessage () {
-  document.getElementById('paragraph').innerHTML = "Hi, my name is Abdullah Al-Rashid"
+  document.getElementById('paragraph').innerHTML = 'Hi, my name is Abdullah Al-Rashid'
 }
 
 document.getElementById('button').addEventListener('click', displayText)
